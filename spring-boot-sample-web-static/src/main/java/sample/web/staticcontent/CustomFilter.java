cb6a6eb94f693993d8148fc1019c0a9c10ce22e3
Emrullah <47065385+Sangaibisi@users.noreply.github.com>
2019-04-08 22:11:05 +0300

    First Commit for proxy server
    Reverse proxy server is using Netflix ZuulProxy that redirect the request from 8080 to 9000

---
+++ spring-boot-sample-web-static/src/main/java/sample/web/staticcontent/CustomFilter.java
@@ -0,0 +1,35 @@
+package sample.web.staticcontent;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class CustomFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        ctx.addZuulRequestHeader("ui", "userservice");
+
+        return null;
+    }
+}
